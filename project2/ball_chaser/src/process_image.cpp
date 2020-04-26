258bdbae2f86fdf89e4e7ae73e936b9a1b8a969d
cjl810 <cameronlomax23@gmail.com>
2020-04-26 00:18:26 -0400

    updated process_image file

--- project2/ball_chaser/src/process_image.cpp
+++ project2/ball_chaser/src/process_image.cpp
@@ -9,6 +9,13 @@ ros::ServiceClient client;
 void drive_robot(float lin_x, float ang_z)
 {
     // TODO: Request a service and pass the velocities to it to drive the robot
+    ball_chaser::DriveToTarget srv;
+    srv.request.linear_x = lin_x;
+    srv.request.angular_z = ang_z;
+
+    if (!srvClient.call(srv)) {
+        ROS_ERROR("Failed to call service to drive_bot");
+    }
 }
 
 // This callback function continuously executes and reads the image data
@@ -21,6 +28,48 @@ void process_image_callback(const sensor_msgs::Image img)
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
+
+
+    // Loops thru to find white pixels (data == 255)
+    int white_pixel_ht = -1, white_pixel_step = -1;
+
+    for (int i = 0; i < img.height * img.step; i++)
+    {
+        if (img.data[i] == 255)
+        {
+            white_pixel_ht = i / img.step;
+            white_pixel_step = i % img.step;
+            break;
+        }
+        
+    }
+
+    // Moves depending on the white ball position
+    float lin_x = 0.0, ang_z = 0.0;
+    if (white_pixel_step <= img.step * 0.3 && white_pixel_step >= 0)
+    {
+        ang_z = 0.5;
+    } else if (white_pixel_step > img.step * 0.7 && white_pixel_step <= img.step)
+    {
+        ang_z = -0.5;
+    } else if (white_pixel_step != -1)
+    {
+        lin_x = 0.5;
+    }
+
+    // Calls drive_bot function and passes velocities to it
+    drive_robot(lin_x, ang_z);
+
+
+    // Publish info
+    if (white_pixel_step != -1)
+    {
+        ROS_INFO_STREAM("Target spotted: moving to target..");
+    } else
+    {
+        ROS_INFO_STREAM("Unable to spot target..");
+    }
+    
 }
 
 int main(int argc, char** argv)
